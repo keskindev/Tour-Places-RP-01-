93b1ade9ab0e7ae756c2d5d8af2dfe73f9ab9a77
keskindev <=>
2023-01-26 22:47:43 +0300

    son commit

--- tour-places/src/App.js
+++ tour-places/src/App.js
@@ -1,11 +1,14 @@
+import "./App.css";
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
+import Cards from "./components/cards/Cards";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Navbar />
       <Header />
+      <Cards />
     </div>
   );
 }
